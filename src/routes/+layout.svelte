0bd90ec96d66f7692702b8c23f62a5b79faf7b4f
mirkoschubert <office@mirkoschubert.de>
2026-05-31 14:54:22 +0200

    fix: inject Umami script via app.html template to guarantee build-time substitution

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,6 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import { systemStore } from '$lib/stores/system.svelte.js';
 	import { onMount } from 'svelte';
-	import { UmamiAnalytics } from '@lukulent/svelte-umami';
-	import { PUBLIC_UMAMI_SRC, PUBLIC_UMAMI_WEBSITE_ID } from '$env/static/public';
 
 	let { children } = $props();
 	let updateReady = $state(false);
@@ -28,8 +26,6 @@
 	<meta name="theme-color" content="#1a1c20" />
 	<title>RetroOS - mirkoschubert</title>
 </svelte:head>
-
-<UmamiAnalytics srcURL={PUBLIC_UMAMI_SRC} websiteID={PUBLIC_UMAMI_WEBSITE_ID} />
 
 {@render children()}
 
